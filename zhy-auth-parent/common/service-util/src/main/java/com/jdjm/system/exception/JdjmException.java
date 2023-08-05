00e22b8154222239b439f7a7786c49fdc31d4af3
‘ZhengHongyi'’ <1111@163.com>
2023-08-05 15:59:59 +0800

    完成批量删除接口、统一异常处理

---
+++ zhy-auth-parent/common/service-util/src/main/java/com/jdjm/system/exception/JdjmException.java
@@ -0,0 +1,18 @@
+package com.jdjm.system.exception;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * 自定义异常
+ */
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class JdjmException extends RuntimeException{
+    private Integer code;
+    private String message;
+}
